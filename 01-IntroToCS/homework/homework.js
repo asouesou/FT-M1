4f07877c1a313fe07d2f2b1a5e02390e3d9a26b5
Hermes <hborasou@gmail.com>
2022-05-23 23:04:48 -0500

    Ejercicio Resueltos S1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,14 +2,27 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  var suma = 0;
+  for (var i = 0; i < num.length; i++)
+  {
+    suma = suma + (num[i] * 2 **(num.length-1 - i));
+  }
+  return suma;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let concatena = "";
 
+  do 
+  {
+    concatena = (num % 2) + concatena;
+  	num = Math.trunc(num/2);
+    console.log(concatena);
+  }while (num > 0);
+   
+  return concatena;
 }
-
 
 module.exports = {
   BinarioADecimal,
